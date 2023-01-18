da5023a774c9fbc8330dc429da90cad6eb1b5ec2
Alam <alam@edenfarm.id>
2023-01-18 15:29:36 +0700

    Read List Done

--- src/store/modules/assignmentSubmission/state.js
+++ src/store/modules/assignmentSubmission/state.js
@@ -1,100 +1,99 @@
 const state = {
-    // Readlist Sales Assignment
+    // Readlist Assignment Submission List
     assignment_submission: {
         id: '',
-        isLoading: false,
+        showFilter : false,
+        loading: false,
         items: [],
         overlay: false,
-        filter:{
-            submitted_date: {
-                model: '',
-                input: new Date(Date.now() + (3600 * 1000 * 7)).toISOString().substr(0, 10),
-                value1: [new Date(Date.now() + (3600 * 1000 * 7)).toISOString().substr(0, 10)],
-                value2: [],
+        statuses: 999,
+        status: [
+            {
+                text: 'All Status',
+                value: 999
             },
-            statuses: 999,
-            status: [
+            {
+                text: 'Finished',
+                value: 2
+            },
+            {
+                text: 'Failed',
+                value: 27
+            }
+        ],
+        table: {
+            fields: [
                 {
-                    text: 'All Status',
-                    value: 999
+                    text: 'Territory',
+                    width: "20%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false,
                 },
                 {
-                    text: 'Finished',
-                    value: 2
+                    text: 'Salesperson',
+                    width: "30%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false,
                 },
                 {
-                    text: 'Failed',
-                    value: 14
-                }
-            ],
-            out_of_route: 999,
-            out_of_route_options: [
-                {
-                    text: 'Show All',
-                    value: 999
+                    text: 'Task',
+                    width: "10%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false
                 },
                 {
-                    text: 'Yes',
-                    value: 1
+                    text: 'Out of Route',
+                    width: "10%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false
                 },
                 {
-                    text: 'No',
-                    value: 0
-                }
+                    text: 'Submitted Date',
+                    width: "15%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false
+                },
+                {
+                    text: 'Finished Date',
+                    width: "15%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false
+                },
+                {
+                    text: 'Status',
+                    width: "5%",
+                    class: 'grey--text text--darken-4',
+                    sortable: false
+                },
+                {
+                    width: "2%",
+                    sortable: false
+                },
             ],
         },
-        table_headers: [
+        out_of_route: 999,
+        out_of_route_options: [
             {
-                text: 'Territory',
-                width: "20%",
-                class: 'grey--text text--darken-4',
-                sortable: false,
+                text: 'Show All',
+                value: 999
             },
             {
-                text: 'Salesperson',
-                width: "30%",
-                class: 'grey--text text--darken-4',
-                sortable: false,
+                text: 'Yes',
+                value: 1
             },
             {
-                text: 'Task',
-                width: "10%",
-                class: 'grey--text text--darken-4',
-                sortable: false
-            },
-            {
-                text: 'Out of Route',
-                width: "10%",
-                class: 'grey--text text--darken-4',
-                sortable: false
-            },
-            {
-                text: 'Submitted Date',
-                width: "15%",
-                class: 'grey--text text--darken-4',
-                sortable: false
-            },
-            {
-                text: 'Finished Date',
-                width: "15%",
-                class: 'grey--text text--darken-4',
-                sortable: false
-            },
-            {
-                text: 'Status',
-                width: "5%",
-                class: 'grey--text text--darken-4',
-                sortable: false
-            },
-            {
-                width: "2%",
-                sortable: false
-            },
+                text: 'No',
+                value: 0
+            }
         ],
         sales_group_id: "",
+        submitted_date_model: '',
+        submitted_date: [new Date(Date.now() + (3600 * 1000 * 7)).toISOString().substr(0, 10)],
+        submitted_date_input: new Date(Date.now() + (3600 * 1000 * 7)).toISOString().substr(0, 10),
         salesperson: '',
         task_type:1,
         clearGroup:false,
+        error: {},
     },
 };
 
