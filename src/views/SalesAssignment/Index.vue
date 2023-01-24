766f7e85bbc9eed998b835fdfacac30a7038fb70
junior <junior@edenfarm.id>
2023-01-24 18:09:57 +0700

    fixdate crm

--- src/views/SalesAssignment/Index.vue
+++ src/views/SalesAssignment/Index.vue
@@ -29,7 +29,7 @@
                                     prepend-inner-icon="mdi-calendar"
                                     outlined
                                     clearable
-                                    @click:clear="start_date.value1 = [],start_date.value2 = [],start_date.input = '',fetchAssignmentList()"
+                                    @click:clear="start_date.value = [],start_date.input = '',fetchAssignmentList()"
                                     v-model="start_date.input"
                                     maxlength="24"
                                     dense
@@ -70,7 +70,7 @@
                                     prepend-inner-icon="mdi-calendar"
                                     outlined
                                     clearable
-                                    @click:clear="end_date.value1 = [],end_date.value2 = [],end_date.input = '',fetchAssignmentList()"
+                                    @click:clear="end_date.value = [],end_date.input = '',fetchAssignmentList()"
                                     v-model="end_date.input"
                                     maxlength="24"
                                     dense
@@ -261,7 +261,7 @@
                         if (val.length == 10) {
                             let valid = this.$moment(val, 'YYYY-MM-DD', true).isValid()
                             if (valid == true) {
-                                this.start_date.value1[0] = this.$moment(val).format('YYYY-MM-DD')
+                                this.start_date.value[0] = this.$moment(val).format('YYYY-MM-DD')
                             }
                         } else if (val.length == 24) {
                             let date1 = val.substr(0, 10)
@@ -269,8 +269,8 @@
                             let valid1 = this.$moment(date1, 'YYYY-MM-DD', true).isValid()
                             let valid2 = this.$moment(date2, 'YYYY-MM-DD', true).isValid()
                             if (valid1 == true && valid2 == true) {
-                                this.start_date.value1[0] = this.$moment(date1).format('YYYY-MM-DD')
-                                this.start_date.value2[1] = this.$moment(date2).format('YYYY-MM-DD')
+                                this.start_date.value[0] = this.$moment(date1).format('YYYY-MM-DD')
+                                this.start_date.value[1] = this.$moment(date2).format('YYYY-MM-DD')
                             }
                         }
                     } else if (val == "") {
@@ -293,7 +293,7 @@
                         if (val.length == 10) {
                             let valid = this.$moment(val, 'YYYY-MM-DD', true).isValid()
                             if (valid == true) {
-                                this.end_date.value1[0] = this.$moment(val).format('YYYY-MM-DD')
+                                this.end_date.value[0] = this.$moment(val).format('YYYY-MM-DD')
                             }
                         } else if (val.length == 24) {
                             let date1 = val.substr(0, 10)
@@ -301,8 +301,8 @@
                             let valid1 = this.$moment(date1, 'YYYY-MM-DD', true).isValid()
                             let valid2 = this.$moment(date2, 'YYYY-MM-DD', true).isValid()
                             if (valid1 == true && valid2 == true) {
-                                this.end_date.value1[0] = this.$moment(date1).format('YYYY-MM-DD')
-                                this.end_date.value2[1] = this.$moment(date2).format('YYYY-MM-DD')
+                                this.end_date.value[0] = this.$moment(date1).format('YYYY-MM-DD')
+                                this.end_date.value[1] = this.$moment(date2).format('YYYY-MM-DD')
                             }
                         }
                     } else if (val == "") {
