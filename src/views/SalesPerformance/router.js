617546b2677ed68dd178e7ff604aa5076dc7fae1
junior <junior@edenfarm.id>
2023-01-24 15:41:46 +0700

    salesperformance

--- src/views/SalesPerformance/router.js
+++ src/views/SalesPerformance/router.js
@@ -4,14 +4,14 @@ export default [
         name: 'SalesPerformance',
         component: () => import("./Index.vue")
     },
-    {
-        path: '/customer-relation/sales-performance/detail/:date/:id',
-        name: 'SalesPerformanceDetail',
-        component: () => import("./Detail")
-    },
     // {
-    //     path: '/customer-relation/sales-performance/detail/:id',
+    //     path: '/customer-relation/sales-performance/detail/:date/:id',
     //     name: 'SalesPerformanceDetail',
     //     component: () => import("./Detail")
     // },
+    {
+        path: '/customer-relation/sales-performance/detail/:id',
+        name: 'SalesPerformanceDetail',
+        component: () => import("./Detail")
+    },
 ]
