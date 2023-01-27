572430e7e6e3e1f00b9cab68ad5173abb6e7107d
Alam <alam@edenfarm.id>
2023-01-27 17:16:36 +0700

    Clean

--- src/store/modules/prospectCustomer/actions.js
+++ src/store/modules/prospectCustomer/actions.js
@@ -75,11 +75,6 @@ const actions = {
             let SelectArchetype = state.prospect_customer.SelectArchetype
             let SelectSalesPerson = state.prospect_customer.SelectSalesPerson
             let SelectRequest = state.prospect_customer.SelectRequest
-            // let API_URL = process.env.VUE_APP_API_URL_BASE;
-            // const response = await axios.get(API_URL+"/bridge/v1/prospective_customer", {
-            //     headers : {
-            //         Authorization : `Bearer ${localStorage.getItem("bearer")}`
-            //     },
             const response = await http.get("/prospective_customer", {
                 params: {
                     per_page:1000,
