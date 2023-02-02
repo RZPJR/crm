fd655bba2011738345dbb46ad8c25f4ce3708208
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2023-02-02 11:36:30 +0700

    slicing crm address detail

--- src/views/Address/Detail.vue
+++ src/views/Address/Detail.vue
@@ -1,37 +1,27 @@
 <template>
     <v-container fill-height class="main-container">
-        <div class="box-start">
-            <v-row>
-                <v-col cols="12" md="3">
-                    <!-- <img
-                        v-if="customer_detail.ktp_photos_url !== null"
-                        class="rounded-form preview-image"
-                        :src="customer_detail.ktp_photos_url[0]"
-                    />
-                    <div v-else :class="'rounded-form text-black60'+imageSizeHandler(customer_detail)"><p>No image</p></div>   
-                    <br v-if="customer_detail.ktp_photos_url !== null"> -->
-                    <div :class="'rounded-form text-black60 image-box'"><p>No image</p></div>
-                    <span class="ml10">KTP</span>             
+        <div class="box">
+            <v-row class="mb2">
+                <v-col cols="12" class="fs24 bold">Customer</v-col>
+                <v-col cols="12" class="fs16 bold">Basic Info</v-col>
+            </v-row>
+            <div class="hr-title mx-1 mb30"/>
+            <v-row class="px-5 mt-5">
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Business Type'" :value="customer_detail.customer?.name"/>
                 </v-col>
-                <v-col cols="12" md="9">
-                    <!-- <div v-if="customer_detail.merchant_photos_url !== null" class="d-flex">
-                        <div v-for="image in customer_detail.merchant_photos_url" :key="image" class="mr2">
-                            <img
-                                class="rounded-form preview-image"
-                                :src="image"
-                            />
-                        </div>
-                    </div>
-                    <div v-else :class="'rounded-form text-black60'+imageSizeHandler(customer_detail)"><p>No image</p></div> -->
-                    <div :class="'rounded-form text-black60 image-box'"><p>No image</p></div>
-                    <span class="ml10">Outlet</span>
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Customer'" :value="customer_detail.customer?.name" :routePath="'/customer-relation/customer/detail/'+this.customer_detail.customer?.id"/>
                 </v-col>
             </v-row>
-            <v-row class="mb24">
-                <v-col class="fs24 bold">
-                    {{customer_detail.name ?? '-'}}
-                </v-col>
-                <v-col class="d-flex justify-end align-end">
+        </div>
+        <div class="box">
+            <v-row>
+                <v-col cols="12" class="fs24 bold">Address</v-col>
+            </v-row>
+            <v-row class="mb2">
+                <v-col class="fs16 bold">Basic Info</v-col>
+                <v-col class="d-flex justify-end -mt5">
                     <v-btn
                         v-if="customer_detail.status == 1"
                         elevation="0"
@@ -39,9 +29,7 @@
                         small
                         class="no-caps mb4"
                         :color="statusMaster('active')"
-                        >
-                        Active
-                    </v-btn>
+                    >Active</v-btn>
                     <v-btn
                         v-if="customer_detail.status == 2"
                         elevation="0"
@@ -49,65 +37,75 @@
                         small
                         class="no-caps mb4"
                         :color="statusMaster('archived')"
-                        >
-                        Archived
-                    </v-btn>
+                    >Archived</v-btn>
                     <v-menu>
                         <template v-slot:activator="{ on }">
-                            <v-btn icon v-on="on">
+                            <v-btn icon v-on="on" class="-mt3">
                                 <v-icon>mdi-dots-vertical</v-icon>
                             </v-btn>
                         </template>
                         <v-list class="bg-white">
-                            <!-- <v-list-item v-privilege="'main_olt_upd'" v-if="customer_detail.status === 1" :to="{ name: 'MerchantUpdate', params: { id: customer_detail.id } }">
+                            <!-- <v-list-item v-privilege="'olt_upd'" :to="{ name: 'BranchUpdate', params: { id: customer_detail.id } }">
                                 <v-list-item-title>Update</v-list-item-title>
                                 <v-list-item-icon><v-icon>mdi-open-in-new</v-icon></v-list-item-icon>
                             </v-list-item>
-                            <v-list-item v-privilege="'main_olt_upd_cust_tag'" v-if="customer_detail.status === 1" :to="{ name: 'MerchantUpdateTag', params: { id: customer_detail.id } }">
-                                <v-list-item-title>Update Tag</v-list-item-title>
+                            <v-list-item v-privilege="'olt_cvt_arc'" :to="`/customer/branch/archetype/convert/${customer_detail.id}`" v-if="items.status == 1 && customer_detail.status === 1">
+                                <v-list-item-title>Convert Archetype</v-list-item-title>
                                 <v-list-item-icon><v-icon>mdi-open-in-new</v-icon></v-list-item-icon>
                             </v-list-item>
-                            <v-list-item v-privilege="'main_olt_upd_pho_num'" v-if="customer_detail.status === 1" :to="{ name: 'MerchantUpdatePhone', params: { id: customer_detail.id } }">
-                                <v-list-item-title>Update Phone</v-list-item-title>
+                            <v-list-item v-privilege="'olt_cvt_arc'" :to="`/customer/branch/convert/${customer_detail.id}`" v-if="items.status != 1 && customer_detail.status === 1">
+                                <v-list-item-title>Convert Archetype</v-list-item-title>
                                 <v-list-item-icon><v-icon>mdi-open-in-new</v-icon></v-list-item-icon>
-                            </v-list-item> -->
+                            </v-list-item>
                             <div>
                                 <hr>
                             </div>
-                            <v-list-item @click="changeStatus(customer_detail.code)" v-privilege="'main_olt_sus'">
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        <span v-if="customer_detail.suspended === 1">Unsuspend</span>
-                                        <span v-else>Suspend</span>
-                                    </v-list-item-title>
-                                </v-list-item-content>
+                            <v-list-item v-privilege="'olt_upd_sps'" @click="showUpdateSalesperson(customer_detail.id)">
+                                <v-list-item-title>Update Salesperson</v-list-item-title>
                             </v-list-item>
-                            <v-list-item v-privilege="'main_olt_urc'" v-if="customer_detail.status === 2" @click="unarchive(customer_detail.id)">
-                                <v-list-item-content>
-                                    <v-list-item-title>Unarchive</v-list-item-title>
-                                </v-list-item-content>
+                            <v-list-item v-privilege="'olt_arc'" @click="archive(customer_detail.id)" v-if="customer_detail.status === 1">
+                                <v-list-item-title>Archive</v-list-item-title>
                             </v-list-item>
-                            <v-list-item v-privilege="'main_olt_arc'" v-if="customer_detail.status === 1" @click="archive(customer_detail.id)">
-                                <v-list-item-content>
-                                    <v-list-item-title>Archive</v-list-item-title>
-                                </v-list-item-content>
+                            <v-list-item @click="unarchive(customer_detail.id)" v-if="customer_detail.status === 2">
+                                <v-list-item-title>Unarchive</v-list-item-title>
                             </v-list-item>
-                            <v-list-item @click="seeHistory()" v-privilege="'filter_rdl'">
-                                <v-list-item-content>
-                                    <v-list-item-title>History</v-list-item-title>
-                                </v-list-item-content>
-                            </v-list-item>
+                            <v-list-item v-privilege="'filter_rdl'" @click="seeHistory()">
+                                <v-list-item-title>See History</v-list-item-title>
+                            </v-list-item> -->
                         </v-list>
                     </v-menu>
                 </v-col>
             </v-row>
-            <v-row>
+            <div class="hr-title mx-1 mb30"/>
+            <v-row class="px-5 mt-5">
                 <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Business Type'" :value="customer_detail.business_type?.name ?? '-'"/>
+                    <DetailRowNew :name="'Address Code'" :value="customer_detail.code ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Code'" :value="customer_detail.code ?? '-'"/>
-                </v-col><v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Address Name'" :value="customer_detail.name ?? '-'"/>
+                </v-col>
+            </v-row>
+            <v-row class="my2">
+                <v-col cols="12" class="fs16 bold">Sales Info</v-col>
+            </v-row>
+            <div class="hr-title mx-1 mb30"/>
+            <v-row class="px-5 mt-5">
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Archetype'" :value="customer_detail.archetype?.name ?? '-'"/>
+                </v-col>
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Salesperson'" :value="customer_detail.salesperson?.name ?? '-'"/>
+                </v-col>
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Price Set'" :value="customer_detail.price_set?.name ?? '-'"/>
+                </v-col>
+            </v-row>
+            <v-row class="my2">
+                <v-col cols="12" class="fs16 bold">Shipping Info</v-col>
+            </v-row>
+            <div class="hr-title mx-1 mb30"/>
+            <v-row class="px-5 mt-5">
+                <v-col cols="12" md="6" class="-mt24">
                     <DetailRowNew :name="'PIC Name'" :value="customer_detail.pic_name ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
@@ -117,96 +115,37 @@
                     <DetailRowNew :name="'Alternative Phone Number'" :value="customer_detail.alt_phone_number ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Email'" :value="customer_detail.email ?? '-'"/>
+                    <DetailRowNew :name="'Province'" :value="customer_detail.sub_district?.district?.city?.province?.name ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Reference Info'" :value="reference_info ?? '-'"/>
-                </v-col>
-                 <v-col cols="12" md="6" class="-mt24">
-                     <!-- <DetailRowNew :name="'Customer Tag'" :value="customer_detail.tag_customer_name ?? '-'" :align="true"/> -->
-                     <DetailRowNew :name="'Customer Tag'" :value="customer_detail.tag_customer_name ?? '-'"/>
+                    <DetailRowNew :name="'City'" :value="customer_detail.sub_district?.district?.city?.name ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
-                    <div v-if="this.customer_detail.referrer">
-                        <DetailRowNew v-if="this.customer_detail.customer_group == 1" :name="'Referrer Name'" :value="customer_detail.referrer.name" :routePath="'/customer/merchant/detail/'+customer_detail.referrer.id"/>
-                        <DetailRowNew v-if="this.customer_detail.customer_group == 2" :name="'Referrer Name'" :value="customer_detail.referrer.name" :routePath="'/customer/agent/detail/'+customer_detail.referrer.id"/>
-                    </div>
-                    <div v-else>
-                        <DetailRowNew :name="'Referrer Name'" :value="'-'"/>
-                    </div>
+                    <DetailRowNew :name="'District'" :value="customer_detail.sub_district?.district?.name ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Suspend'" :value="customer_detail.suspended === 1 ? 'Yes' : 'No'"/>
+                    <DetailRowNew :name="'Sub District'" :value="customer_detail.sub_district?.name ?? '-'"/>
                 </v-col>
                 <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Referral Code'" :value="customer_detail.referral_code ?? '-'"/>
+                    <DetailRowNew :name="'Postal Code'" :value="customer_detail.sub_district?.postal_code ?? '-'"/>
+                </v-col>
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Default Warehouse'" :value="customer_detail.warehouse?.name ?? '-'"/>
+                </v-col>
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Area'" :value="customer_detail.sub_district?.area?.name ?? '-'"/>
+                </v-col>
+                <v-col cols="12" md="6" class="-mt24">
+                    <DetailRowNew :name="'Default Shipping Address'" :value="customer_detail.main_branch === 1 ? 'Yes' : customer_detail.main_branch === 2 ? 'No' : '-'"/>
+                </v-col>
+                <v-col cols="12" class="-mt24">
+                    <DetailRowNew :name="'Shipping Address'" :value="customer_detail.shipping_address ?? '-'" :align="true"/>
                 </v-col>
                 <v-col cols="12" class="-mt24">
                     <DetailRowNew :name="'Note'" :value="customer_detail.note ?? '-'" :align="true"/>
                 </v-col>
             </v-row>
         </div>
-        <div class="box-start">
-            <v-row class="mb24">
-                <v-col class="fs16 bold">Finance Info</v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Finance Area'" :value="customer_detail.finance_area?.name ?? '-'"/>
-                </v-col>
-                <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Default Payment Term'" :value="customer_detail.payment_term?.name ?? '-'"/>
-                </v-col>
-                <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Default Invoice Term'" :value="customer_detail.invoice_term?.name ?? '-'"/>
-                </v-col>
-                <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Credit Limit'" :value="customer_detail.credit_limit_amount > 0 ? formatPrice(customer_detail.credit_limit_amount) : '-'"/>
-                </v-col>
-                <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Business Type Credit Limit'" :value="customer_detail.business_type_credit_limit === 1 ? 'Badan Usaha' : 'Personal'"/>
-                </v-col>
-                <v-col cols="12" md="6" class="-mt24">
-                    <DetailRowNew :name="'Remaining Credit Limit'" :value="handleRemainingCreditLimitAmount() ? formatPrice(customer_detail.remaining_credit_limit_amount??0) : '-'"/>
-                </v-col>
-                <v-col cols="12" class="-mt24">
-                    <DetailRowNew :name="'Billing Address'" :value="customer_detail.billing_address ?? '-'" :align="true"/>
-                </v-col>
-            </v-row>
-        </div>
-        <div class="box-header-table -mt4">
-            Payment Info
-        </div>
-        <div class="box-body-table">
-            <v-data-table
-                :mobile-breakpoint="0"
-                :headers="customer_detail.table_header"
-                :items="customer_detail.merchant_acc_num"
-                :items-per-page="10"
-            >
-                <template v-slot:item="props">
-                    <tr>
-                        <td>{{ props.index + 1 }}</td>
-                        <td>{{ props.item.payment_channel.name }}</td>
-                        <td>{{ props.item.account_name }}</td>
-                        <td>{{ props.item.account_number }}</td>
-                    </tr>
-                </template>
-            </v-data-table>
-        </div>
-        <div style="padding: 20px;" class="row d-flex align-end justify-end">
-            <v-btn
-                rounded
-                elevation="0"
-                class="no-caps px-7"
-                :to="'/customer/prospect-customer'"
-                style="background: #E6E9ED; color: #768F9C; height: 45px;"
-            >
-                Back
-            </v-btn>
-        </div>
-        <!-- <ConfirmationDialogNew :sendData="ConfirmData"/> -->
-        <LoadingBar :value="customer_detail.isLoading"/>
     </v-container>
 </template>
 
@@ -214,14 +153,14 @@
     import { mapState, mapActions } from "vuex";
 
     export default {
-        name: 'CustomerDetail',
+        name: 'AddressDetail',
         data() {
             return {
-                reference_info: '',
+                // 
             }
         },
         mounted() {
-            this.fetchCustomerDetail()
+            this.renderData()
         },
         computed: {
             ...mapState({
@@ -230,57 +169,11 @@
         },
         methods: {
             ...mapActions ([
-                'fetchCustomerDetail',
+                'fetchAddressDetail',
             ]),
-            // Handling image size by class
-            imageSizeHandler(item){ 
-                return item.ktp_photos_url === null && item.merchant_photos_url === null ? ' image-box' : ' image-box-big'
-            },
-            // Hide show remaining credit limit
-            handleRemainingCreditLimitAmount() { 
-                if (this.customer_detail.remaining_credit_limit_amount === 0) {
-                    if (this.customer_detail.credit_limit_amount === 0) {
-                        return false
-                    } else {
-                        return true
-                    }
-                } else {
-                    return true
-                }
+            renderData(){
+                this.fetchAddressDetail({id: this.$route.params.id});
             },
         }
     }
 </script>
-
-<style scoped>
-    .preview-image {
-        width: 100%;
-        height: 250px;
-        object-fit: cover;
-    }
-    
-    .image-box {
-        width:250px;
-        height:50px;
-        background: #EBEBEB;
-        border-style:dashed;
-        border-color:gray;
-        border-width:1px;
-        text-align: center;
-        padding-top: 10px;
-        margin: 10px;
-    }
-    
-    .image-box-big {
-        width:250px;
-        height:250px;
-        background: #EBEBEB;
-        border-style:dashed;
-        border-color:gray;
-        border-width:1px;
-        text-align: center;
-        padding-top: 110px;
-        margin: 0px 0px 10px 10px;
-    }
-
-</style>
