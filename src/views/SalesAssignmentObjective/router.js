ba409cda9b8e0865eabd30713a73c6d8d054b629
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2023-02-03 15:03:18 +0700

    breadcrumbs

--- src/views/SalesAssignmentObjective/router.js
+++ src/views/SalesAssignmentObjective/router.js
@@ -3,15 +3,45 @@ export default [
         path: '/customer-relation/sales-assignment-objective',
         name: 'SalesAssignmentObjective',
         component: () => import("./Index.vue"),
+        meta: {
+            auth: true,
+            title: 'Sales Assignment Objective List'
+        },
     },
     {
         path: '/customer-relation/sales-assignment-objective/create',
         name: 'SalesAssignmentObjectiveCreate',
         component: () => import("./Create.vue"),
+        meta: {
+            auth: true,
+            title: 'Sales Assignment Objective Create',
+            breadcrumbs : [
+                {
+                    text: 'Sales Assignment Objective List',
+                    to : '/customer-relation/sales-assignment-objective'
+                },
+                {
+                    text: 'Sales Assignment Objective Create',
+                },
+            ],
+        }
     },
     {
         path: '/customer-relation/sales-assignment-objective/update/:id',
         name: 'SalesAssignmentObjectiveUpdate',
         component: () => import("./Update.vue"),
+        meta: {
+            auth: true,
+            title: 'Sales Assignment Objective Update',
+            breadcrumbs : [
+                {
+                    text: 'Sales Assignment Objective List',
+                    to : '/customer-relation/sales-assignment-objective'
+                },
+                {
+                    text: 'Sales Assignment Objective Update',
+                },
+            ],
+        }
     },
 ]
